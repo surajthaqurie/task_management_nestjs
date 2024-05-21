10b85bcbc9482ce711f00cb451ee96406e3837bf
Suraj <surajchan68@gmail.com>
2024-05-21 21:00:27 +0545

    fix: add user controller conflict

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,6 @@
-<<<<<<< HEAD
-import {
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  UseGuards,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import {
-  ApiBearerAuth,
-  ApiHeader,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
-=======
 import { Controller, Delete, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiHeader, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
->>>>>>> f30d0ea (feat: add new remote)
 import { JwtGuard } from 'src/auth/guards';
 import { USER_CONSTANT } from 'src/constant';
 import { UserResponseDto } from './dto';
@@ -33,96 +14,6 @@ import { IUser } from './interfaces';
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-<<<<<<< HEAD
-  constructor(private readonly userService: UserService) {}
-
-  @Get()
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({
-    summary: 'Get all users.',
-    description: 'Authenticated user can get all user list',
-  })
-  @ApiBearerAuth()
-  @ApiHeader({
-    name: 'Bearer',
-    description: 'The token we need for auth',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    type: UserResponseDto,
-    isArray: true,
-    description: USER_CONSTANT.USERS_FETCHED_SUCCESS,
-  })
-  async getUsers(): Promise<AppResponse<IUser[]>> {
-    const users = await this.userService.getUsers();
-
-    return new AppResponse<IUser[]>(USER_CONSTANT.USERS_FETCHED_SUCCESS)
-      .setStatus(200)
-      .setSuccessData(users);
-  }
-
-  @Get('profile')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({
-    summary: 'Get user profile.',
-    description: 'Authenticated user get their profile',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    type: UserResponseDto,
-    description: USER_CONSTANT.USER_PROFILE_FETCHED_SUCCESS,
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: COMMON_ERROR.UNAUTHORIZED,
-  })
-  @ApiBearerAuth()
-  @ApiHeader({
-    name: 'Bearer',
-    description: 'The token we need for auth',
-  })
-  async getUserById(
-    @getUser() currentUser: IJwtResponse,
-  ): Promise<AppResponse<IUser | null>> {
-    const user = await this.userService.getUserById(currentUser.id);
-    return new AppResponse<IUser | null>(
-      USER_CONSTANT.USER_PROFILE_FETCHED_SUCCESS,
-    )
-      .setStatus(200)
-      .setSuccessData(user);
-  }
-
-  @Delete(':id')
-  @UseGuards(JwtGuard)
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({
-    summary: 'Delete user.',
-    description: 'Authenticated user can delete their account',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    type: UserResponseDto,
-    description: USER_CONSTANT.USER_DELETED_SUCCESS,
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: COMMON_ERROR.UNAUTHORIZED,
-  })
-  @ApiBearerAuth()
-  @ApiHeader({
-    name: 'Bearer',
-    description: 'The token we need for auth',
-  })
-  async deleteUser(
-    @getUser() currentUser: IJwtResponse,
-  ): Promise<AppResponse<IUser | null>> {
-    const user = await this.userService.deleteUser(currentUser.id);
-
-    return new AppResponse<IUser | null>(USER_CONSTANT.USER_DELETED_SUCCESS)
-      .setStatus(200)
-      .setSuccessData(user);
-  }
-=======
     constructor(private readonly userService: UserService) {}
 
     @Get()
@@ -187,5 +78,4 @@ export class UserController {
 
         return new AppResponse<IUser | null>(USER_CONSTANT.USER_DELETED_SUCCESS).setStatus(200).setSuccessData(user);
     }
->>>>>>> f30d0ea (feat: add new remote)
 }
